62e9b9a1376c19d12104c8a154b7078b27ab0d80
Ashfat Al Rashid <ashfat.rashid@polygontech.xyz>
2024-11-18 15:21:34 +0600

    Links Test Added

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.By;
 public class ElementsPage extends HomePage {
 
     private By webTablesMenuItem = By.xpath("//div[@class='element-list collapse show']//li[@id='item-3']");
+    private By linksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
 
     public WebTablesPage clickWebTables(){
         click(webTablesMenuItem);
         return new WebTablesPage();
+    }
+
+    public LinksPage clickLinks(){
+        click(linksMenuItem);
+        return new LinksPage();
     }
 }
