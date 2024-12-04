2d274592aa3069a77860844d0ff49e5a109829f4
Ashfat Al Rashid <ashfat.rashid@polygontech.xyz>
2024-12-04 15:19:36 +0600

    Date Test Modified

--- src/test/java/part3_4/com/demoqa/tests/part3/widgets/DateTest.java
+++ src/test/java/part3_4/com/demoqa/tests/part3/widgets/DateTest.java
@@ -7,19 +7,20 @@ import part3_4.com.demoqa.base.BaseTest;
 public class DateTest extends BaseTest {
 
     String month = "November";
+    String monthNumber = "11";
     String day = "25";
     String year = "2024";
 
     @Test
     public void testSelectingDate() {
         var datePickerPage = homePage.goToWidgets().clickDatePicker();
-        datePickerPage.clickDatePicker();
+        datePickerPage.clickSelectDate();
         datePickerPage.selectMonth(month);
         datePickerPage.selectYear(year);
         datePickerPage.clickDay(day);
 
         String actualDate = datePickerPage.getDate();
-        String expectedDate = month + "/" + day + "/" + year;
+        String expectedDate = monthNumber + "/" + day + "/" + year;
         Assert.assertEquals(actualDate, expectedDate, "\n Actual & Expected date don't match" +
                                                                "\n Actual Date: " + actualDate +
                                                                "\n Expected Date: " + expectedDate);
